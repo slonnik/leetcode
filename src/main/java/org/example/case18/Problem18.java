5d91c07c3442df5701ed84d1f41146654210d851
aleks683 <al36400pa>
2022-06-22 17:27:04 +0300

    Solve problem 18

--- src/main/java/org/example/case18/Problem18.java
+++ src/main/java/org/example/case18/Problem18.java
@@ -8,6 +8,28 @@ import org.example.util.TreeNode;
 public class Problem18 {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return false;
+        return internalCheck(p, q);
+    }
+
+    public boolean internalCheck(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null && q != null) {
+            return false;
+        }
+
+        if (p != null && q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        if (p.left == null && p.right == null && q.left == null && q.right == null) {
+            return true;
+        }
+        return internalCheck(p.right, q.right) && internalCheck(p.left, q.left);
     }
 }
--- src/test/java/org/example/case18/Problem18Test.java
+++ src/test/java/org/example/case18/Problem18Test.java
@@ -1,7 +1,33 @@
 package org.example.case18;
 
 
+import org.example.util.TreeNode;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Problem18Test {
 
+    @Test
+    public void testPositive1() {
+        var node1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        var node2 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        assertTrue(new Problem18().isSameTree(node1, node2));
+    }
 
+    @Test
+    public void testNegative1() {
+        var node1 = new TreeNode(1, new TreeNode(2), null);
+        var node2 = new TreeNode(1, null, new TreeNode(2));
+        assertFalse(new Problem18().isSameTree(node1, node2));
+    }
+
+    @Test
+    public void testNegative2() {
+        var node1 = new TreeNode(1, new TreeNode(1), new TreeNode(2));
+        var node2 = new TreeNode(1, new TreeNode(2), new TreeNode(1));
+        assertFalse(new Problem18().isSameTree(node1, node2));
+    }
 }
