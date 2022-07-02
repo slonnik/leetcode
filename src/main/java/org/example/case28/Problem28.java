cd3dbb5a035c895c585f47e5856a0ae948c06f59
slonnik <slonnik@gmail.com>
2022-07-02 22:09:55 +0300

    resolve problem 28

--- src/main/java/org/example/case28/Problem28.java
+++ src/main/java/org/example/case28/Problem28.java
@@ -5,20 +5,29 @@ package org.example.case28;
  */
 public class Problem28 {
     public int singleNumber(int[] nums) {
-        final int shift = 3 * 10_000;
-        final int size = 6 * 10_1000 + 1;
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        final int size = max - min + 1;
+
         boolean[] data = new boolean[size];
         for (int i = 0; i < nums.length; ++i) {
-            int pos = nums[i] + shift;
-            if (data[pos] == true) {
-                data[pos] = false;
-            } else {
-                data[pos] = true;
-            }
+            int pos = nums[i] - min;
+            data[pos] = !data[pos];
         }
         for (int i = 0; i < size; ++i) {
             if (data[i]) {
-                return i - shift;
+                return i + min;
             }
         }
         return -1;
