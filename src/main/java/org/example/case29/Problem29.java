444f33d1919f1b69db66d9557b739a40c4130465
palamarchuk <slonnik@gmail.com>
2022-07-05 20:19:25 +0300

    Solve Problem29

--- src/main/java/org/example/case29/Problem29.java
+++ src/main/java/org/example/case29/Problem29.java
@@ -2,11 +2,27 @@ package org.example.case29;
 
 import org.example.util.ListNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/linked-list-cycle/
  */
 public class Problem29 {
     public boolean hasCycle(ListNode head) {
+        if(head == null) {
+            return false;
+        }
+        final int MAX_LENGTH = 10_010;
+        int count = 1;
+        while(head.next != null) {
+            count++;
+            if(count > MAX_LENGTH)
+            {
+                return  true;
+            }
+            head = head.next;
+        }
         return false;
     }
 }
