19ac39a33f65944624f645cef3ca3464e6df442e
slonnik <slonnik@gmail.com>
2022-07-24 18:17:53 +0300

    resolve problem 28

--- src/main/java/org/example/case33/Problem33.java
+++ src/main/java/org/example/case33/Problem33.java
@@ -1,6 +1,6 @@
 package org.example.case33;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/missing-ranges/
@@ -8,30 +8,30 @@ import java.util.List;
 public class Problem33 {
 
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
-        //int notInLength = nums[nums.length - 1] - nums[0];
-        //int[] notInData = new int[notInLength];
-        //int minValue = Math.min(nums[0], lower);
-        //int maxValue = Math.max(nums[nums.length - 1], upper);
+
+        var result = new LinkedHashMap<Integer, Deque<Integer>>();
         int index = 0;
-        int count = 0;
         for (int i = lower; i <= upper; ++i) {
-            if (index >= nums.length) {
-                System.out.println(i);
-                continue;
-            }
-            int value = nums[index];
+            int value = (index < nums.length) ? nums[index] : upper + 1;
             if (i < value) {
-                count++;
-                System.out.println(i);
+                var deque = result.getOrDefault(value, new LinkedList<>());
+                deque.add(i);
+                result.put(value, deque);
             } else if (i == value) {
-                int end = value - 1;
-                int start = value - count;
-                count = 0;
                 index++;
-                System.out.println(String.format("%d->%d", start, end));
             }
         }
-        return List.of();
+
+        List<String> resultList = new LinkedList<>();
+        for (Map.Entry<Integer, Deque<Integer>> entry : result.entrySet()) {
+            var deque = entry.getValue();
+            if (deque.size() == 1) {
+                resultList.add(String.format("%d", deque.element()));
+            } else {
+                resultList.add(String.format("%d->%d", deque.getFirst(), deque.getLast()));
+            }
+        }
+        return resultList;
     }
 
 
